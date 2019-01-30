6100a01467702e913cd9796a240fcc3e9665dcec
nicor88 <nico@8fit.com>
2019-01-30 15:50:30 +0100

    some refinement..

--- taxi_uploader.py
+++ taxi_uploader.py
@@ -24,10 +24,11 @@ BASE_URL = 'https://s3.amazonaws.com/nyc-tlc/trip+data'
 
 
 def save_to_s3(*, source_url, s3_key, s3_bucket):
-    destination_bucket = s3.Bucket(s3_bucket)
+    destination_bucket_resource = s3.Bucket(s3_bucket)
     global transfer_size_bytes
     transfer_size_bytes = 0
     file_size_bytes = int(requests.head(source_url).headers.get('Content-Length'))
+    file_size_megabytes = round(file_size_bytes / (1024 * 1024), 2)
 
     def _print_transferred_bytes(bytes_transferred):
         global transfer_size_bytes
@@ -38,14 +39,18 @@ def save_to_s3(*, source_url, s3_key, s3_bucket):
         print(f'Transferred {transfer_size_bytes} out of {file_size_bytes} bytes -  {transfer_percentage}%')
 
     def _upload_to_s3():
-        print(f'Starting the upload to s3://{s3_bucket}/{s3_key}')
-        destination_obj = destination_bucket.Object(s3_key)
+
+        destination_obj = destination_bucket_resource.Object(s3_key)
         with requests.get(source_url, stream=True) as response:
             destination_obj.upload_fileobj(response.raw, Callback=_print_transferred_bytes)
             transfer_size_megabytes = round(transfer_size_bytes / (1024 * 1024), 2)
-            print(f'{s3_key} saved to S3, total size: {transfer_size_bytes} bytes, {transfer_size_megabytes} MB')
+            print(f's3://{s3_bucket}/{s3_key} imported successfully, total size: {transfer_size_bytes} bytes, {transfer_size_megabytes} MB')
 
-    print(f'File size {source_url} is {file_size_bytes} bytes')
+    print(f'File size {source_url} is {file_size_bytes} bytes, {file_size_megabytes} MB')
+
+    # this string is going to be overridden
+    print(f'Starting the upload to s3://{s3_bucket}/{s3_key}')
+
     _upload_to_s3()
 
 
@@ -54,7 +59,7 @@ def generate_months(start_month_str: str, end_month_str: str):
     end_month = parser.parse(f'{end_month_str}-01')
     diff = relativedelta.relativedelta(start_month, end_month)
 
-    month_diff = abs(diff.months)+1
+    month_diff = abs(diff.months) + 1
 
     for month in range(month_diff):
         yield (start_month + relativedelta.relativedelta(months=month)).strftime('%Y-%m')
@@ -71,7 +76,7 @@ def get_data_set(data_set_types, start_month, end_month, destination_bucket_name
             url = f'{BASE_URL}/{filename}'
             year = year_month.split('-')[0]
             month = year_month.split('-')[1]
-            hive_partition_key = get_hive_partition_key('new_york_taxi', 'fhv_tripdata', year, month, filename)
+            hive_partition_key = get_hive_partition_key('new_york_taxi', data_set, year, month, filename)
 
             save_to_s3(source_url=url, s3_bucket=destination_bucket_name, s3_key=hive_partition_key)
 
@@ -91,10 +96,10 @@ def create_argparser():
 
 if __name__ == '__main__':
     args = create_argparser().parse_args()
-    start_month_str = vars(args).get('start_month')
-    end_month_str = vars(args).get('end_month')
+    start_month_input = vars(args).get('start_month')
+    end_month_input = vars(args).get('end_month')
     destination_bucket = vars(args).get('destination_bucket')
 
     data_sets = ['fhv_tripdata', 'green_tripdata', 'yellow_tripdata']
 
-    get_data_set(data_sets, start_month_str, end_month_str, destination_bucket)
+    get_data_set(data_sets, start_month_input, end_month_input, destination_bucket)
